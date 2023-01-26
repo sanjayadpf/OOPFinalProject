ebdf12cbc4c357184be4f20592b91b88c066a040
Sanjaya Mallikarachchi <sanjayadpf@gmail.com>
2023-01-26 15:53:25 -0600

    PaintCanvas paint method content comment out

--- src/view/gui/PaintCanvas.java
+++ src/view/gui/PaintCanvas.java
@@ -14,7 +14,7 @@ public class PaintCanvas extends JComponent {
         // For example purposes only; remove all lines below from your final project.
         // Draw all shapes here
        
-        graphics2d.setColor(Color.GREEN);
+      /*  graphics2d.setColor(Color.GREEN);
         graphics2d.fillRect(12, 13, 200, 400);
 
         // Outlined rectangle
@@ -26,7 +26,7 @@ public class PaintCanvas extends JComponent {
         Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[]{9}, 0);
         graphics2d.setStroke(stroke);
         graphics2d.setColor(Color.BLACK);
-        graphics2d.drawRect(7, 8, 210, 410);
+        graphics2d.drawRect(7, 8, 210, 410); */
      
     }
 }
