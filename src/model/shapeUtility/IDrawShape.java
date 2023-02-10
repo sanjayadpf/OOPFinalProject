2651cabeb5336fb348b48db217b0d4d4db9cfbee
Sanjaya Mallikarachchi <sanjayadpf@gmail.com>
2023-02-10 12:09:17 -0600

    IDrawShape interface for Strategy design pattern. RectangleStrategy wth overidden draw method.

---
+++ src/model/shapeUtility/IDrawShape.java
@@ -0,0 +1,8 @@
+package model.shapeUtility;
+
+import java.awt.Graphics2D;
+
+public interface IDrawShape {
+	
+	public void draw(CreateShape shape, Graphics2D graphics2d);
+}
---
+++ src/model/shapeUtility/RectangleStrategy.java
@@ -0,0 +1,33 @@
+package model.shapeUtility;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import model.ShapeShadingType;
+
+public class RectangleStrategy implements IDrawShape {
+
+    @Override
+    public void draw(CreateShape shape, Graphics2D graphics2d) {
+
+        if (shape.shadingType == ShapeShadingType.OUTLINE) {
+            graphics2d.setColor(Color.WHITE);
+            graphics2d.fillRect(shape.x, shape.y, shape.width, shape.height);
+            graphics2d.setStroke(new BasicStroke(5));
+            graphics2d.setColor(shape.secondaryColor);
+            graphics2d.drawRect(shape.x, shape.y, shape.width, shape.height);
+        } else if (shape.shadingType == ShapeShadingType.FILLED_IN) {
+            graphics2d.setColor(shape.primaryColor);
+            graphics2d.fillRect(shape.x, shape.y, shape.width, shape.height);
+            graphics2d.setStroke(new BasicStroke(5));
+            graphics2d.setColor(shape.primaryColor);
+            graphics2d.drawRect(shape.x, shape.y, shape.width, shape.height);
+        } else if (shape.shadingType == ShapeShadingType.OUTLINE_AND_FILLED_IN) {
+            graphics2d.setColor(shape.primaryColor);
+            graphics2d.fillRect(shape.x, shape.y, shape.width, shape.height);
+            graphics2d.setStroke(new BasicStroke(5));
+            graphics2d.setColor(shape.secondaryColor);
+            graphics2d.drawRect(shape.x, shape.y, shape.width, shape.height);
+        }
+    }
+}
