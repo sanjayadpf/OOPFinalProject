2163774ae17e9b56a58baf49b6b3a96da0adf7e0
Sanjaya Mallikarachchi <sanjayadpf@gmail.com>
2023-02-25 22:12:40 -0600

    static method implementations in CommandFactory for draw,select

--- src/controller/CommandFactory.java
+++ src/controller/CommandFactory.java
@@ -5,10 +5,28 @@
  */
 package controller;
 
+import controller.interfaces.ICommand;
+import java.awt.Color;
+import model.ShapeShadingType;
+import model.ShapeType;
+import model.shapeUtility.GenerateShape;
+
 /**
  *
  * @author sanja
  */
 public class CommandFactory {
-    
+
+    public static ICommand draw(int x, int y, Point startPoint, Point endPoint,
+            int height, int width,ShapeType shapeType, Color primaryColor, Color secondaryColor, ShapeShadingType shadingType) {
+        return new GenerateShape(x, y, startPoint, endPoint, height, width,shapeType, primaryColor, secondaryColor, shadingType);
+    }
+
+    public static ICommand select(Point startPoint, Point endPoint) {
+        return new SelectCommand(startPoint, endPoint);
+    }
+
+   /* public static ICommand move(CustomPair startingPair) {
+        return new MoveShape(startingPair);
+    }*/
 }
