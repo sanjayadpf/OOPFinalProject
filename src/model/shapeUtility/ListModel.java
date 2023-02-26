ddffa3bda9ce3ed4cd33d2e60f4833d1d2f325ac
Sanjaya Mallikarachchi <sanjayadpf@gmail.com>
2023-02-25 23:00:57 -0600

    ListModel updates to contain selected and clipboard elements

--- src/model/shapeUtility/ListModel.java
+++ src/model/shapeUtility/ListModel.java
@@ -5,6 +5,7 @@
  */
 package model.shapeUtility;
 
+import model.interfaces.IShapeList;
 import model.interfaces.IShapeSubject;
 
 /**
@@ -14,5 +15,47 @@ import model.interfaces.IShapeSubject;
 public class ListModel {
 
     public static IShapeSubject subjectList;
+    private static IShapeList selectedList;
+    private static IShapeList clipBoard;
+    
+    static ListModel model;
+
+	public ListModel(IShapeSubject subjectList, IShapeList selectedList, IShapeList clipBoard) {
+		ListModel.subjectList = subjectList;
+		ListModel.selectedList = selectedList;
+		ListModel.clipBoard = clipBoard;
+	}
+
+    public static IShapeSubject getSubjectList() {
+        return subjectList;
+    }
+
+    public static void setSubjectList(IShapeSubject subjectList) {
+        ListModel.subjectList = subjectList;
+    }
+
+    public static IShapeList getSelectedList() {
+        return selectedList;
+    }
+
+    public static void setSelectedList(IShapeList selectedList) {
+        ListModel.selectedList = selectedList;
+    }
+
+    public static IShapeList getClipBoard() {
+        return clipBoard;
+    }
+
+    public static void setClipBoard(IShapeList clipBoard) {
+        ListModel.clipBoard = clipBoard;
+    }
+
+    public static ListModel getModel() {
+        return model;
+    }
+
+    public static void setModel(ListModel model) {
+        ListModel.model = model;
+    }
  
 }
